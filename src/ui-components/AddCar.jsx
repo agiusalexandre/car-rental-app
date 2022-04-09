976f00bbfd29d904f7ee723289b606569074b239
Alexandre Agius <aaagius@amazon.fr>
2022-04-09 15:14:44 +0200

    commit III

---
+++ src/ui-components/AddCar.jsx
@@ -0,0 +1,190 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import React from "react";
+import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import { Button, Divider, Flex, Text, TextField } from "@aws-amplify/ui-react";
+export default function AddCar(props) {
+  const { overrides, ...rest } = props;
+  return (
+    <Flex
+      gap="16px"
+      direction="column"
+      width="640px"
+      position="relative"
+      padding="0px 0px 0px 0px"
+      backgroundColor="rgba(255,255,255,1)"
+      {...rest}
+      {...getOverrideProps(overrides, "AddCar")}
+    >
+      <Flex
+        gap="24px"
+        direction="column"
+        shrink="0"
+        alignSelf="stretch"
+        objectFit="cover"
+        position="relative"
+        padding="24px 24px 24px 24px"
+        {...getOverrideProps(overrides, "Content")}
+      >
+        <Flex
+          gap="16px"
+          direction="row"
+          alignItems="center"
+          shrink="0"
+          alignSelf="stretch"
+          objectFit="cover"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          {...getOverrideProps(overrides, "Edit Profile")}
+        >
+          <Text
+            fontFamily="Inter"
+            fontSize="16px"
+            fontWeight="400"
+            color="rgba(13,26,38,1)"
+            lineHeight="20px"
+            textAlign="left"
+            display="flex"
+            direction="column"
+            justifyContent="flex-start"
+            shrink="0"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            whiteSpace="pre-wrap"
+            children="Add Car"
+            {...getOverrideProps(overrides, "Add Car")}
+          ></Text>
+        </Flex>
+        <Divider
+          height="1px"
+          shrink="0"
+          alignSelf="stretch"
+          objectFit="cover"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          size="small"
+          orientation="horizontal"
+          {...getOverrideProps(overrides, "Dividerdgs")}
+        ></Divider>
+        <Flex
+          gap="16px"
+          direction="column"
+          shrink="0"
+          alignSelf="stretch"
+          objectFit="cover"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          {...getOverrideProps(overrides, "Forms")}
+        >
+          <TextField
+            display="flex"
+            gap="8px"
+            direction="column"
+            justifyContent="center"
+            shrink="0"
+            alignSelf="stretch"
+            objectFit="cover"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            label="name"
+            placeholder="Toyota - Prius"
+            size="default"
+            isDisabled={false}
+            labelHidden={false}
+            variation="default"
+            {...getOverrideProps(overrides, "TextFieldklw")}
+          ></TextField>
+          <TextField
+            display="flex"
+            gap="8px"
+            direction="column"
+            justifyContent="center"
+            shrink="0"
+            alignSelf="stretch"
+            objectFit="cover"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            label="Price (USD)"
+            placeholder="20"
+            size="default"
+            isDisabled={false}
+            labelHidden={false}
+            variation="default"
+            {...getOverrideProps(overrides, "TextFieldico")}
+          ></TextField>
+          <TextField
+            display="flex"
+            gap="8px"
+            direction="column"
+            justifyContent="center"
+            shrink="0"
+            alignSelf="stretch"
+            objectFit="cover"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            label="Description"
+            placeholder="4 Seats"
+            size="default"
+            isDisabled={false}
+            labelHidden={false}
+            variation="default"
+            {...getOverrideProps(overrides, "TextFielduhd")}
+          ></TextField>
+          <TextField
+            display="flex"
+            gap="8px"
+            direction="column"
+            justifyContent="center"
+            shrink="0"
+            alignSelf="stretch"
+            objectFit="cover"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            label="image link"
+            placeholder="image URL"
+            size="default"
+            isDisabled={false}
+            labelHidden={false}
+            variation="default"
+            {...getOverrideProps(overrides, "TextFieldbhi")}
+          ></TextField>
+        </Flex>
+        <Divider
+          height="1px"
+          shrink="0"
+          alignSelf="stretch"
+          objectFit="cover"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          size="small"
+          orientation="horizontal"
+          {...getOverrideProps(overrides, "Dividerwcv")}
+        ></Divider>
+        <Button
+          display="flex"
+          gap="0"
+          direction="row"
+          width="fit-content"
+          justifyContent="center"
+          alignItems="center"
+          shrink="0"
+          position="relative"
+          border="1px SOLID rgba(0,0,0,0)"
+          borderRadius="4px"
+          padding="7px 15px 7px 15px"
+          backgroundColor="rgba(4,125,149,1)"
+          size="default"
+          isDisabled={false}
+          variation="primary"
+          children="Save"
+          {...getOverrideProps(overrides, "Button")}
+        ></Button>
+      </Flex>
+    </Flex>
+  );
+}
---
+++ src/ui-components/NavBar.jsx
@@ -0,0 +1,143 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import React from "react";
+import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import { Button, Flex, Image, Text } from "@aws-amplify/ui-react";
+export default function NavBar(props) {
+  const { overrides, ...rest } = props;
+  return (
+    <Flex
+      gap="20px"
+      direction="row"
+      width="1440px"
+      justifyContent="center"
+      alignItems="center"
+      position="relative"
+      padding="24px 32px 24px 32px"
+      backgroundColor="rgba(255,255,255,1)"
+      {...rest}
+      {...getOverrideProps(overrides, "NavBar")}
+    >
+      <Flex
+        gap="2px"
+        direction="row"
+        width="fit-content"
+        height="46px"
+        justifyContent="center"
+        alignItems="center"
+        shrink="0"
+        alignSelf="stretch"
+        position="relative"
+        padding="0px 0px 0px 0px"
+        {...getOverrideProps(overrides, "Logo")}
+      >
+        <Image
+          width="45px"
+          height="45px"
+          shrink="0"
+          position="relative"
+          borderRadius="160px"
+          padding="0px 0px 0px 0px"
+          {...getOverrideProps(overrides, "imagezzp")}
+        ></Image>
+        <Text
+          fontFamily="Inter"
+          fontSize="20px"
+          fontWeight="400"
+          color="rgba(0,0,0,1)"
+          textTransform="capitalize"
+          lineHeight="23.4375px"
+          textAlign="left"
+          display="flex"
+          direction="column"
+          justifyContent="flex-start"
+          shrink="0"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          whiteSpace="pre-wrap"
+          children="Car Rental"
+          {...getOverrideProps(overrides, "Car Rental")}
+        ></Text>
+      </Flex>
+      <Flex
+        gap="40px"
+        direction="row"
+        width="594px"
+        alignItems="center"
+        grow="1"
+        basis="594px"
+        height="24px"
+        position="relative"
+        padding="0px 0px 0px 0px"
+        {...getOverrideProps(overrides, "Frame 321mky")}
+      >
+        <Text
+          fontFamily="Inter"
+          fontSize="16px"
+          fontWeight="400"
+          color="rgba(0,0,0,1)"
+          lineHeight="24px"
+          textAlign="left"
+          display="flex"
+          direction="column"
+          justifyContent="flex-start"
+          letterSpacing="0.01px"
+          shrink="0"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          whiteSpace="pre-wrap"
+          children="Dashboard"
+          {...getOverrideProps(overrides, "Dashboard")}
+        ></Text>
+      </Flex>
+      <Flex
+        gap="32px"
+        direction="row"
+        width="594px"
+        justifyContent="flex-end"
+        alignItems="center"
+        grow="1"
+        basis="594px"
+        height="46px"
+        position="relative"
+        padding="0px 0px 0px 0px"
+        {...getOverrideProps(overrides, "Frame 321aad")}
+      >
+        <Button
+          display="flex"
+          gap="0"
+          direction="row"
+          width="fit-content"
+          justifyContent="center"
+          alignItems="center"
+          shrink="0"
+          alignSelf="stretch"
+          position="relative"
+          border="1px SOLID rgba(0,0,0,0)"
+          borderRadius="4px"
+          padding="7px 15px 7px 15px"
+          backgroundColor="rgba(4,125,149,1)"
+          size="large"
+          isDisabled={false}
+          variation="primary"
+          children="Sign out"
+          {...getOverrideProps(overrides, "Button")}
+        ></Button>
+        <Image
+          width="45px"
+          height="45px"
+          shrink="0"
+          position="relative"
+          borderRadius="160px"
+          padding="0px 0px 0px 0px"
+          {...getOverrideProps(overrides, "imageqat")}
+        ></Image>
+      </Flex>
+    </Flex>
+  );
+}
